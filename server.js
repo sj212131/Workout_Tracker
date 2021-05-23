55e7521237a5f99f49781edcf5c604cbb5647cd0
shangxian Piao <sj212131@hotmail.com>
2021-05-23 13:44:09 -0400

    upload models, routes and server file

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+
+const apiRoutes = require("./routes/api.js");
+const htmlRoutes = require("./routes/htmlRoutes.js");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(morgan("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+});
+
+app.use(apiRoutes);
+app.use(htmlRoutes);
+
+app.listen(PORT, function () {
+  console.log(`App listening on Port ${PORT}`);
+});
